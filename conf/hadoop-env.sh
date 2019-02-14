9fc5aad478e65139b698e97a84252ff36edbe0da
fuyao <397400733@qq.com>
2019-02-14 16:49:31 +0800

    abc

---
+++ conf/hadoop-env.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr/java/latest
+export HADOOP_CONF_DIR=/home/hadoop/data/hadoop/etc/hadoop
---
+++ conf/hbase-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export JAVA_HOME=/usr/java/latest
+export HBASE_CLASSPATH=/home/hadoop/data/hadoop/etc/hadoop
+export HBASE_MANAGES_ZK=false
+export HBASE_LOG_DIR=/home/hadoop/data/hbase/logs
